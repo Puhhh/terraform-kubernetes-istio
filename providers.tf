b59e35e7049467ceec28edf0553ea08c0f1cd0d8
Aleksei Blinov <alvablinov@gmail.com>
2024-09-20 10:32:56 +0300

    update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,11 +2,6 @@ terraform {
   required_version = ">= 0.13"
 
   required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
